8333ada4fd2110efa59a4ed146df393e28c4d55e
Amanda M. Jessup <mandajessup@gmail.com>
2021-02-27 15:06:59 -0500

    Add project files.

---
+++ ElephantExercise/Elephant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantExercise
+{
+    class Elephant
+    {
+        public int earSize;
+        public string name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("\nMy name is " + name);
+            Console.WriteLine("My ears are " + earSize + " inches tall");
+
+        }
+
+        public void HearMessage(string message, Elephant whoSaidit)
+        {
+            Console.WriteLine(name + " heard a message");
+            Console.WriteLine(whoSaidit.name + " said this: " + message);
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+
+
+
+    }
+}
---
+++ ElephantExercise/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ElephantExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { name = "Lucinda", earSize = 33 };
+            Elephant lloyd = new Elephant() { name = "Lloyd", earSize = 40 };
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Press:\n\n[1] for Lloyd,\n[2] for Lucinda,\n[3] to Swap");
+                byte response = byte.Parse(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine("You pressed: " + response);
+
+                if (response == 1)
+                {
+                    lloyd.WhoAmI();
+                    // lloyd.HearMessage("Hi", lucinda);
+                    lucinda.SpeakTo(lloyd, "Hi, Lloyd!");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+                else if (response == 2)
+                {
+                    lucinda.WhoAmI();
+                    lloyd.SpeakTo(lucinda, "Hi Lucinda!");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+                else if (response == 3)
+                {
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                    Console.WriteLine("References have been swapped");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+                else return;
+                //Console.WriteLine();
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
